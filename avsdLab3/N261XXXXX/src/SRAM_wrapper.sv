47bc7cbe036a01e9956ae043b0f08ddf9d71677d
haha7 <casper24723902@gmail.com>
2025-11-10 02:58:11 +0800

    Refactor SRAM wrapper for improved readability
    - Reorganized the SRAM wrapper to enhance readability
      and remove redundant logic.
    - Overall functionality remains unchanged, except for
      the counter mechanism: previously, the counter was
      updated differently for read and write operations
      (read path was one cycle faster).
    - The new version unifies the counter behavior for both
      read and write operations by adjusting SRAM_A in the ReadData
      state  within the SRAM interface to achieve the same effect.

--- avsdLab3/N261XXXXX/src/SRAM_wrapper.sv
+++ avsdLab3/N261XXXXX/src/SRAM_wrapper.sv
@@ -45,30 +45,33 @@ module SRAM_wrapper(
     input  logic                            BREADY_S
 );
 
-	// ============================================================
-	// Local signals and parameters
-	// ============================================================
+	//====================================================
+    // State Definition
+    //====================================================
 	typedef enum logic [1:0] {
 		ACCEPT        = 2'd0,
 		ReadData      = 2'd1,
 		WriteData     = 2'd2,
 		WriteResponse = 2'd3
-	} trans_state_t;
+	} state_t;
 
+	state_t CurrentState, NextState;
 
-	trans_state_t 				CurrentState, NextState;
+	//====================================================
+    // Local Signals and Registers
+    //====================================================
 	logic [`AXI_IDS_BITS-1:0] 	AWID, ARID;
-	logic [`AXI_ADDR_BITS-1:0] 	RWADDR, A_tmp;
-	logic [`AXI_LEN_BITS-1:0] 	RWLEN;
-	logic [`AXI_SIZE_BITS-1:0]  RWSIZE;
-	logic [1:0] 				RWBURST;
-	logic [`AXI_LEN_BITS-1:0]   RWcount;
-	logic [`AXI_DATA_BITS-1:0]  ReadBuffer;
-	logic 						Waiting, RAddrLAST;
-	logic 						CEB, WEB;
-	logic [`AXI_DATA_BITS-1:0]  BWEB;
-	logic [`AXI_DATA_BITS-1:0] 	DI, DO;
-	logic [13:0] 				A;
+	logic [`AXI_LEN_BITS-1:0] 	LEN;
+	logic [`AXI_LEN_BITS-1:0]   LEN_cnt;
+	logic [`AXI_ADDR_BITS-1:0] 	ADDR;
+
+	logic 						buf_VALID;
+	logic [`AXI_DATA_BITS-1:0]  buf_SRAM_Q;
+
+	logic 						SRAM_CEBn, SRAM_WEBn;
+	logic [`AXI_DATA_BITS-1:0]  SRAM_BWEBn;
+	logic [13:0] 				SRAM_A;
+	logic [`AXI_DATA_BITS-1:0] 	SRAM_D, SRAM_Q;
 
 	// ============================================================
 	// Finite State Machine
@@ -88,209 +91,146 @@ module SRAM_wrapper(
 	always_comb begin
         case(CurrentState)
         ACCEPT: begin
-            if (ARVALID_S)          	  NextState = ReadData;
-            else if (AWVALID_S)     	  NextState = WriteData;
-            else                          NextState = ACCEPT;
+            if (ARVALID_S)      NextState = ReadData;
+            else if (AWVALID_S) NextState = WriteData;
+            else                NextState = ACCEPT;
         end
         ReadData: begin
             if (RVALID_S && RREADY_S && RLAST_S)
-                                          NextState = ACCEPT;
-            else                          NextState = CurrentState;
+                                NextState = ACCEPT;
+            else                NextState = CurrentState;
         end
         WriteData: begin
             if (WVALID_S && WREADY_S && WLAST_S)
-                                          NextState = WriteResponse;
-            else                          NextState = CurrentState;
+                                NextState = WriteResponse;
+            else                NextState = CurrentState;
         end
         WriteResponse: begin
             if(BVALID_S && BREADY_S)
-                                          NextState = ACCEPT;
-            else                          NextState = CurrentState;
+                                NextState = ACCEPT;
+            else                NextState = CurrentState;
         end
         endcase
     end
 
 	// ============================================================
-	// Address calculation logic
-	// ============================================================
+    // Channel Output Logic (combinational)
+    // ============================================================
 	always_comb begin
-		if ((CurrentState == ACCEPT) && ARVALID_S)
-			A_tmp = (ARADDR_S) >> 2;
-		else
-			A_tmp = {1'b0, (RWADDR[31:2] + {26'd0, RWcount})};
-
-		A = A_tmp[13:0];
-	end
-	// ============================================================
-	// Channel logic
-	// ============================================================
-
-	// ---------------------------------------
-    // ACCEPT
-    // ---------------------------------------
-    assign ARREADY_S = (CurrentState == ACCEPT);
-    assign AWREADY_S = (CurrentState == ACCEPT);
-
-	// ---------------------------------------
-    // ReadData
-    // ---------------------------------------
-	always_comb begin
-		if (CurrentState == ReadData) begin
-			RID_S 		= ARID;
-			RDATA_S 	= (Waiting) ? ReadBuffer : DO;
-			RRESP_S 	= `AXI_RESP_OKAY;
-			RVALID_S 	= 1'b1;
-			RLAST_S 	= RAddrLAST;
-		end
-		else begin
-			RID_S 		= `AXI_IDS_BITS'd0;
-			RDATA_S 	= `AXI_DATA_BITS'd0;
-			RRESP_S 	= `AXI_RESP_DECERR;
-			RVALID_S	= 1'b0;
-			RLAST_S 	= 1'b0;
-		end
-	end
-
-	// ---------------------------------------
-    // WriteData
-    // ---------------------------------------
-	always_comb begin
-		if (CurrentState == WriteData) begin
-			WREADY_S 	= 1'b1;
-			DI 			= WDATA_S;
-			BWEB 		= {{8{~WSTRB_S[3]}}, {8{~WSTRB_S[2]}}, {8{~WSTRB_S[1]}}, {8{~WSTRB_S[0]}}};
-			WEB 		= 1'b0;
-		end
-		else begin
-			WREADY_S 	= 1'b0;
-			DI 			= 32'd0;
-			BWEB 		= 32'hFFFF_FFFF;
-			WEB 		= 1'b1;
-		end
-	end
-
-	// ---------------------------------------
-    // WriteResponse
-    // ---------------------------------------
-	always_comb begin
-		if (CurrentState == WriteResponse) begin
-			BID_S 		= AWID;
-			BVALID_S 	= 1'b1;
-			BRESP_S 	= `AXI_RESP_OKAY;
-		end
-		else begin
-			BID_S 		= `AXI_IDS_BITS'd0;
-			BVALID_S 	= 1'b0;
-			BRESP_S 	= `AXI_RESP_DECERR;
-		end
+		ARREADY_S   = 1'b0;
+		AWREADY_S   = 1'b0;
+		RID_S       = `AXI_IDS_BITS'd0;
+		RDATA_S     = `AXI_DATA_BITS'd0;
+		RRESP_S     = `AXI_RESP_DECERR;
+		RVALID_S    = 1'b0;
+		RLAST_S     = 1'b0;
+		WREADY_S    = 1'b0;
+		BID_S       = `AXI_IDS_BITS'd0;
+		BVALID_S    = 1'b0;
+		BRESP_S     = `AXI_RESP_DECERR;
+		case (CurrentState)
+			ACCEPT: begin
+				ARREADY_S = 1'b1;
+				AWREADY_S = 1'b1;
+			end
+			ReadData: begin
+				RID_S     = ARID;
+				RDATA_S   = (buf_VALID) ? buf_SRAM_Q : SRAM_Q;
+				RRESP_S   = `AXI_RESP_OKAY;
+				RVALID_S  = 1'b1;
+				RLAST_S   = (LEN_cnt == LEN);
+			end
+			WriteData: begin
+				WREADY_S  = 1'b1;
+			end
+			WriteResponse: begin
+				BID_S     = AWID;
+				BVALID_S  = 1'b1;
+				BRESP_S   = `AXI_RESP_OKAY;
+			end
+		endcase
 	end
 
 	// ============================================================
-	// Sequential storage logic
+	// Request Information Storage
 	// ============================================================
 	always_ff @( posedge ACLK or negedge ARESETn ) begin
 		if (~ARESETn) begin
-			ARID 	<= `AXI_IDS_BITS'd0;
-            AWID 	<= `AXI_IDS_BITS'd0;
-            RWADDR 	<= `AXI_ADDR_BITS'd0;
-            RWLEN 	<= `AXI_LEN_BITS'd0;
-            RWSIZE 	<= `AXI_SIZE_BITS'd0;
-            RWBURST <= 2'd0;
-		end
-		else begin
-			if ((CurrentState == ACCEPT) && ARVALID_S) begin
-				ARID  	<= ARID_S;
-				RWADDR 	<= ARADDR_S;
-				RWLEN 	<= ARLEN_S;
-				RWSIZE 	<= ARSIZE_S;
-				RWBURST <= ARBURST_S;
-			end
-			else if ((CurrentState == ACCEPT) && AWVALID_S) begin
-				AWID  	<= AWID_S;
-				RWADDR 	<= AWADDR_S;
-				RWLEN 	<= AWLEN_S;
-				RWSIZE 	<= AWSIZE_S;
-				RWBURST <= AWBURST_S;
-			end
+			ARID <= `AXI_IDS_BITS'd0;
+			AWID <= `AXI_IDS_BITS'd0;
+			ADDR <= `AXI_ADDR_BITS'd0;
+			LEN  <= `AXI_LEN_BITS'd0;
+		end else if(CurrentState == ACCEPT)begin
+			ARID <= (ARVALID_S) ? ARID_S  : ARID;
+			AWID <= (AWVALID_S) ? AWID_S  : AWID;
+			LEN  <= (ARVALID_S) ? ARLEN_S : (AWVALID_S ? AWLEN_S  : LEN);
+			ADDR <= (ARVALID_S) ? ARADDR_S: (AWVALID_S ? AWADDR_S : ADDR);
 		end
 	end
 
 	// ============================================================
 	// Counter logic
 	// ============================================================
-	always_ff @(posedge ACLK or negedge ARESETn) begin // RWcount
-		if (!ARESETn) begin
-			RWcount   <= `AXI_LEN_BITS'd0;
-			RAddrLAST <= 1'b0;
-		end
-		else begin
-			// ------------------------------------------------
-			// Case 1: Data transfer (read or write in progress)
-			// ------------------------------------------------
-			if ((RVALID_S && RREADY_S) || (WVALID_S && WREADY_S)) begin
-				if ((RWcount == RWLEN) || RAddrLAST) begin
-					RWcount   <= `AXI_LEN_BITS'd0;
-					RAddrLAST <= 1'b1;
-				end
-				else begin
-					RWcount   <= RWcount + `AXI_LEN_BITS'd1;
-					RAddrLAST <= 1'b0;
-				end
-			end
-
-			// ------------------------------------------------
-			// Case 2: Idle state receiving new read request
-			// ------------------------------------------------
-			else if ((CurrentState == ACCEPT) && ARVALID_S) begin
-				if (RWcount == ARLEN_S) begin
-					RWcount   <= `AXI_LEN_BITS'd0;
-					RAddrLAST <= 1'b1;
-				end
-				else begin
-					RWcount   <= RWcount + `AXI_LEN_BITS'd1;
-					RAddrLAST <= 1'b0;
-				end
-			end
+	always_ff @(posedge ACLK or negedge ARESETn) begin
+		if (~ARESETn) begin
+			LEN_cnt <= `AXI_LEN_BITS'd0;
+		end else if ((RVALID_S && RREADY_S) || (WVALID_S && WREADY_S)) begin
+			LEN_cnt <= (LEN_cnt == LEN) ? `AXI_LEN_BITS'd0 : LEN_cnt + `AXI_LEN_BITS'd1;
 		end
 	end
 
-
-	always_ff @( posedge ACLK or negedge ARESETn ) begin // Waiting
-		if (!ARESETn) Waiting <= 1'b0;
-		else begin
-			if ((CurrentState == ReadData) && ~RREADY_S)
-				  Waiting <= 1'b1;
-			else  Waiting <= 1'b0;
+	// ============================================================
+	// Buffer for ReadData
+	// ============================================================
+	always_ff @( posedge ACLK or negedge ARESETn ) begin // Pending
+		if (~ARESETn) begin
+			buf_VALID  <= 1'b0;
+			buf_SRAM_Q <= `AXI_DATA_BITS'b0;
+		end else if (RVALID_S && ~RREADY_S) begin
+			buf_VALID  <= 1'b1;
+			buf_SRAM_Q <= SRAM_Q;
+		end else if (RVALID_S && RREADY_S) begin
+			buf_VALID  <= 1'b0;
+			buf_SRAM_Q <= `AXI_DATA_BITS'b0;
 		end
 	end
-
-	always_ff @( posedge ACLK or negedge ARESETn ) begin // ReadBuffer
-		if (!ARESETn) ReadBuffer <= `AXI_DATA_BITS'd0;
-		else begin
-			if ((CurrentState == ReadData) && Waiting)
-				  ReadBuffer <= ReadBuffer;
-			else  ReadBuffer <= DO;
-		end
-	end
-
-	always_ff @( posedge ACLK or negedge ARESETn ) begin // CurrentState NextState
-		if (~ARESETn) CEB <= 1'b0;
-	end
+	// ============================================================
+	// SRAM Interface
+	// ============================================================
+	always_comb begin
+		SRAM_CEBn  = 1'b0;
+		SRAM_WEBn  = 1'b1;
+		SRAM_BWEBn = 32'hFFFF_FFFF;
+		SRAM_D     = 32'd0;
+		SRAM_A     = 14'd0;
+		case (CurrentState)
+            ACCEPT:
+                SRAM_A 	   = ARVALID_S ? ARADDR_S[15:2] : 14'd0;
+            ReadData:
+                SRAM_A 	   = ADDR[15:2] + {10'd0, LEN_cnt + `AXI_LEN_BITS'd1};
+            WriteData : begin
+				SRAM_WEBn  = 1'b0;
+                SRAM_BWEBn = {{8{~WSTRB_S[3]}}, {8{~WSTRB_S[2]}}, {8{~WSTRB_S[1]}}, {8{~WSTRB_S[0]}}};
+				SRAM_A	   = ADDR[15:2] + {10'd0, LEN_cnt};
+				SRAM_D     = WDATA_S;
+            end
+        endcase
+    end
 
 TS1N16ADFPCLLLVTA512X45M4SWSHOD i_SRAM (
-    .SLP(1'b0),
-    .DSLP(1'b0),
-    .SD(1'b0),
-    .PUDELAY(),
-    .CLK(ACLK),
-	.CEB(CEB),
-	.WEB(WEB),
-    .A(A),
-	.D(DI),
-    .BWEB(BWEB),
-    .RTSEL(2'b01),
-    .WTSEL(2'b01),
-    .Q(DO)
+    .SLP		(1'b0		),
+    .DSLP		(1'b0		),
+    .SD			(1'b0		),
+    .PUDELAY	(			),
+    .CLK		(ACLK		),
+	.CEB		(SRAM_CEBn	),
+	.WEB		(SRAM_WEBn	),
+    .A			(SRAM_A		),
+	.D			(SRAM_D		),
+    .BWEB		(SRAM_BWEBn	),
+    .RTSEL		(2'b01		),
+    .WTSEL		(2'b01		),
+    .Q			(SRAM_Q		)
 );
 
 
